e2fa81e7da3dec388548832d0513bc41f6d17938
Dries Decuyper <decuyperdries@gmail.com>
2023-09-11 14:35:29 +0200

    ucll specific

--- app/src/main/java/com/example/blankproject/MainActivity.kt
+++ app/src/main/java/com/example/blankproject/MainActivity.kt
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     BlankProjectTheme {
-        Greeting("Android")
+        Greeting("Android UCLL")
     }
 }
